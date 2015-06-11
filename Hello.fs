d3530df1036217ba0f4b37bed30b5dcf5903fd5a
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-06-11 21:42:07 +0700

    rename file

---
+++ Hello.fs
@@ -0,0 +1,12 @@
+
+open System
+
+module Test =
+  let x = 100
+  let y = 200
+  let z = 300
+
+let x = Test.x
+let y = Test.y
+let z = Test.z
+
--- test.fs
+++ test.fs
@@ -10,6 +10,5 @@ let rs' = testBox 100
 let rs'' = testBox "Hello"
 let rs''' = testBox (DateTime(2015,10,10))
 
-
 let double' = [1;2;3;4] |> List.map (fun x -> x + x)
 
